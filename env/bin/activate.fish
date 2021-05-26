f1ec1050170d782e103b4e6b41f6d43d2a93d7c0
smoothzz <thiago.rdoa@outlook.com>
2021-05-26 18:05:46 +0000

    fix system

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ec2-user/horas/env"
+set -gx VIRTUAL_ENV "/home/ubuntu/horas/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
